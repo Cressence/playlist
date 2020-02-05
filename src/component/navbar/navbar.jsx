4fad930268743294d68c61f15e98f857d77e594a
Cressence <cressencey@gmail.com>
2020-02-06 00:02:11 +0100

    Complate Navbar

---
+++ src/component/navbar/navbar.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Menu, Search } from 'semantic-ui-react';
+
+export default function Navbar() {
+    const [activeItem, setActiveItem] = useState('');
+
+    const handleItemClick = (e, { name }) => setActiveItem(name);
+
+    return (
+        <div>
+            <Menu className="navbar-section" stackable secondary>
+                <Menu.Item
+                    name='PLAYLIST'
+                    active={activeItem === 'logo'}
+                    onClick={handleItemClick}
+                />
+                <Menu.Item
+                    name='Genre'
+                    active={activeItem === 'home'}
+                    onClick={handleItemClick}
+                />
+                <Menu.Item
+                    name='Artist'
+                    active={activeItem === 'messages'}
+                    onClick={handleItemClick}
+                />
+                <Menu.Item
+                    name='Albums'
+                    active={activeItem === 'friends'}
+                    onClick={handleItemClick}
+                />
+                <Menu.Menu position='right'>
+                    <Menu.Item>
+                        <Search icon='search' placeholder='Search...' />
+                    </Menu.Item>
+                </Menu.Menu>
+            </Menu>
+        </div>
+    )
+}
---
+++ src/layout/main/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Navbar from './../../component/navbar/navbar';
+
+function Main() {
+    return (
+        <div className="main-body">
+            <Navbar />
+        </div>
+    )
+}
+
+export default Main;
