d086bd21a33c0ef61ef371b22fff3e0c17eb9042
Cressence <cressencey@gmail.com>
2020-02-06 23:55:43 +0100

    Setup main page

--- src/layout/main/main.jsx
+++ src/layout/main/main.jsx
@@ -6,6 +6,19 @@ function Main() {
     return (
         <div className="main-body">
             <Navbar />
+
+            <div className="body-container">
+                <div>
+                    <h3 className="white">Browse</h3>
+                    <p className="grey small-text">Explore by genre</p>
+                    <div>
+                        <div className="single-genre">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
