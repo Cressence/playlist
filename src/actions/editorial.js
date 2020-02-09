51687edeba6340f8d2386c5ae94c1c61fe03dd19
Cressence <cressencey@gmail.com>
2020-02-09 22:21:04 +0100

    Start connecting editorial

--- src/actions/editorial.js
+++ src/actions/editorial.js
@@ -3,24 +3,24 @@ import {
     EDITORIAL, API_FAIL
 } from './../constants/constants';
 
-// function getLocalizationPromise(language) {
-//     return axios.get(`${LOCALIZATION_URL}/applications/41/locals/${language}/keys`)
-//         .then(data => {
-//             return data;
-//         })
-//         .catch(err => {
-//             return (err.response);
-//         });
-// }
+function getLocalizationPromise(language) {
+    return axios.get(`https://deezerdevs-deezer.p.rapidapi.com/editorial/132`)
+        .then(data => {
+            return data;
+        })
+        .catch(err => {
+            return (err.response);
+        });
+}
 
-// export const getLocalization = (language) => (dispatch) => {
-//     return getLocalizationPromise(language)
-//         .then(resp => dispatch({
-//             type: EDITORIAL,
-//             data: resp
-//         }))
-//         .catch(resp => dispatch({
-//             type: API_FAIL,
-//             data: resp
-//         }))
-// };
+export const getLocalization = (language) => (dispatch) => {
+    return getLocalizationPromise(language)
+        .then(resp => dispatch({
+            type: EDITORIAL,
+            data: resp
+        }))
+        .catch(resp => dispatch({
+            type: API_FAIL,
+            data: resp
+        }))
+};
