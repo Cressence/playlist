a27331a756d45287d2db89f4ce445483f30b9a5e
Cressence <cressencey@gmail.com>
2020-04-02 14:34:42 +0100

    Complete Track component

--- src/component/track/track.jsx
+++ src/component/track/track.jsx
@@ -9,15 +9,19 @@ import './track.css';
 
 function Track(props) {
     return (
-        <Modal trigger={props.trigger} size="small" className="track-background">
+        <Modal trigger={props.trigger} size="small">
             <Modal.Content>
                 <Modal.Description>
                     <Header>{props.music.title}</Header>
+                    <img src={require('./../../assets/img/music.gif')} alt="music-icon" className="music-gif" />
                     <AudioPlayer
                         autoPlay
                         src={props.music.preview}
                         onPlay={e => console.log("onPlay")}
                     />
+                    <br />
+                    <p><strong>{props.music.artist.name}</strong></p>
+                    <p>Listen to complete track on <a target="_blank" alt="deezer-music" rel="noopener noreferrer" href={props.music.link}>Deezer</a></p>
                 </Modal.Description>
             </Modal.Content>
         </Modal>
