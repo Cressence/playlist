6a68f82bf7494c493805414397e794674400e6eb
Cressence <cressencey@gmail.com>
2020-04-05 21:38:14 +0100

    Setup view playlist layout

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Main from './layout/main/main';
-import ViewAlbum from './layout/viewAlbum/viewAlbum';
+import ViewPlaylist from './layout/viewPlaylist/viewpPlaylist';
 
 // import styles
 import './layout/main/main.css';
@@ -13,7 +13,7 @@ function App() {
   return (
     <Router>
       <Route exact path="/" component={Main} />
-      <Route path="/album" component={ViewAlbum} />
+      <Route path="/playlist" component={ViewPlaylist} />
     </Router>
   );
 }
---
+++ src/layout/viewPlaylist/viewpPlaylist.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './../../component/navbar/navbar';
+
+function ViewPlaylist() {
+    return (
+        <div className={"main-body"}>
+            <Navbar />
+            
+        </div>
+    )
+}
+
+export default ViewPlaylist;
