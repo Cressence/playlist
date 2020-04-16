9b3c8f347ee3212c7ff91a92e0ca94fd05112ae0
Cressence <cressencey@gmail.com>
2020-04-16 22:14:13 +0100

    Setup grid for layout

--- src/layout/viewPlaylist/viewpPlaylist.jsx
+++ src/layout/viewPlaylist/viewpPlaylist.jsx
@@ -3,22 +3,22 @@ import { Grid } from 'semantic-ui-react';
 import Navbar from './../../component/navbar/navbar';
 
 function ViewPlaylist(props) {
+    // redirect to home page when site opens from playlist page 
+    let playlistInfo = (props.location.state !== undefined) ? props.location.state.playlist: props.history.push('/');
+    
     const [width, setWidth] = useState(0);
-
     let columnSize = (width <= 432) ? 1 : 2;
 
     const updatePageWidth = () => {
         setWidth(window.innerWidth);
     }
     useLayoutEffect(() => {
-        // redirect to home page when site opens from playlist page
-        const playlistInfo = (props.location.state !== undefined) ? props.location.state.playlist: props.history.push('/');
         updatePageWidth();
         window.addEventListener('resize', updatePageWidth);
         updatePageWidth();
         return () => window.removeEventListener('resize', updatePageWidth);
-    }, [props]);
-    console.log(props)
+    }, [props,  playlistInfo]);
+    console.log(playlistInfo)
     return (
         <div className={"main-body"}>
             <Navbar />
